d0b8f028763a7ee7f15d52f1a12ccbe4c6914267
Данила Акимов <ceylar37@gmail.com>
2021-11-18 15:46:00 +0300

    build script redaction

--- README.md
+++
@@ -1,7 +0,0 @@
-# Exchange Rates
-
-## Technoligies:
-React, Redux, jQuery, Redux Toolkit, Electron, Ant Design
-
-### Description:
-Десктоп приложение показывающее текущие курсы валют.
--- src/store/index.ts
+++
@@ -1,9 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit";
-import {currencyReducer} from "./reducers/currency";
-
-export const store = configureStore({
-    reducer: {currency: currencyReducer}
-})
-
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
